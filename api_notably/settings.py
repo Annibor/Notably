43e8990ee8706efc0fdbd517be2fda75069aca4a
Bumbi <annika.borgesson@gmail.com>
2024-05-21 12:59:49 +0200

    add in secret key #6

--- api_notably/settings.py
+++ api_notably/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&zx4vx!zhf+c*s8r=zn!&v$_+&5&9fg0_3=0=glq!*4q6o+)4)"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
